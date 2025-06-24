4a313c5e33db3b071cfe4d1f66d666e4606163ce
hyunkikomori <jhs880514@naver.com>
2025-06-24 21:17:02 +0900

    스토리 헤더 파일 수정
    한글 폰트 깨지던 것 수정
    주석 추가

--- Spartale/Source/GameLogic/Story/Story.h
+++ Spartale/Source/GameLogic/Story/Story.h
@@ -4,21 +4,20 @@
 #include <chrono>
 #include <thread>
 
-// ���丮 ���� ��� �Լ���
-void Scene_Prologue(); // ���ѷα�(����)
-void Scene_Begin();   // ������ ���� - �������� ���.
-void Scene_Field();   // �ʵ� ù ���� ��
-void Scene_TowerGate(); // ž�� �� �տ���
-void Scene_FinalBoss(); // �������� ���� �� ���. ������ ���� ���� ����.
-void Scene_Epiloge(); // ���ʷα�(����)
+// 스토리 관련 폰트
+void Scene_Prologue(); // 프롤로그(오프닝)
+void Scene_Begin();   // 마을에서 께어남 - 끝나면 게임 플레이 시작.
+//void Scene_Field();   // 현재는 사용 중지
+void Scene_TowerGate(); // 탑의 정문에 들어가기 직전
+void Scene_FinalBoss(); // 최상층에서 보스와 조우. 끝나면 최종보스전 시작.
+void Scene_Epiloge(); // 에필로그(엔딩)
 
 void typeEffect(std::string text, int delay = 30);
 
 void waitForEnter() {
-	std::cout << "\n[Enter�� ���� ����ϼ���...]";
-	std::cin.ignore();
-	std::cin.get();
-	std::cout << std::endl;
+	std::cout << "\n[Enter를 눌러 계속하세요...]";
+	std::cin.ignore(); // 이전 버퍼 비우기
+	std::cin.get();    // 사용자 입력 기다리기
 }
 
 void typeEffect(std::string text, int delay = 30) {
